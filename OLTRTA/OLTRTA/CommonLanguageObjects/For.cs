43e89395f4bcf4d29bbba17c2f318db5c9cefd3c
ToreMartin <ToreMartin@TORE-LAPTOP>
2017-04-24 13:17:21 +0200

    Added language objects

---
+++ OLTRTA/OLTRTA/CommonLanguageObjects/For.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLTRTA.CommonLanguageObjects {
+    class For : Expression {
+        Expression init;
+        Expression check;
+        Expression action;
+        Block bl;
+    }
+}
---
+++ OLTRTA/OLTRTA/CommonLanguageObjects/Method.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLTRTA.CommonLanguageObjects {
+    class Method {
+        string type;
+        Parameter[] parameters;
+        Block bl;
+    }
+}
